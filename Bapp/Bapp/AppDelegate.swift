4414ca1b59308557b429e2877dfe24739532be15
hi-hu <mike@wealthfront.com>
2015-05-25 13:03:33 -0700

    parse fake login

--- Bapp/Bapp/AppDelegate.swift
+++ Bapp/Bapp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,9 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("AWQBh1JE2ecoJRB9uVAGn0JDR71ovzGxBV0VWxpS", clientKey: "j5kLboZgCr5pyJvs2uNOlUtdbplDvKQTVJ3PnCXf")
+        
         return true
     }
-
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- Bapp/Bapp/LoginViewController.swift
+++ Bapp/Bapp/LoginViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +26,26 @@ class LoginViewController: UIViewController {
     }
     
 
+    @IBAction func loginDidPress(sender: AnyObject) {
+        
+        PFUser.logInWithUsernameInBackground(usernameField.text, password: passwordField.text ) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+                println("login success")
+            } else {
+                // The login failed. Check error to see why.
+                println("login fail")
+            }
+        }
+        
+        
+        
+        
+    }
+
+    
     /*
     // MARK: - Navigation
 
